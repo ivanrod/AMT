5e5aa2447da8718da2b701c708ca7b219744c48b
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-11-17 10:50:45 +0100

    Added course & user models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Amt::Application.routes.draw do
 
   root 'welcome#index'
+
+  get 'courses' => 'courses#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141117085439) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.integer  "estimated_hours"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
