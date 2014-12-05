1e041ee1c3c686a86473739f1eada8ec81056470
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-12-05 13:47:39 +0100

    assets bug fixed4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,8 @@
 Rails.application.config.assets.precompile += ['foundation-icons.css','planification.js', 'planification.css']
 
 #Foundation Icons
-Rails.application.config.assets.precompile += ['foundation-icons.css', 'foundation-icons.woff','foundation-icons.ttf']
+Rails.application.config.assets.precompile += Dir["app/assets/stylesheets/foundation-icons/*"]
+
+#Vendor
+Rails.application.config.assets.precompile += Dir["vendor/assets/javascripts/*"]
+Rails.application.config.assets.precompile += Dir["vendor/assets/stylesheets/*"]
