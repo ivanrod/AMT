d72d466abd15d1b67a11a74de71b44d4b5f1db2f
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-12-05 19:00:58 +0100

    assets bug fixed4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.precompile += ['foundation-icons.css','planifica
 #Foundation Icons
 array_to_slice = Dir["app/assets/stylesheets/foundation-icons/*"]
 array_to_slice.each do |x|
-  x.slice! "app/assets/stylesheets/foundation-icons/"
+  x.slice! "app/assets/stylesheets/"
 end
 
 Rails.application.config.assets.precompile += array_to_slice
@@ -19,5 +19,17 @@ Rails.application.config.assets.precompile += array_to_slice
 array_to_slice = Dir["vendor/assets/stylesheets/*"]
 array_to_slice.each do |x|
   x.slice! "vendor/assets/stylesheets/"
+end
+Rails.application.config.assets.precompile += array_to_slice
+
+#Javascripts & stylesheets
+array_to_slice = Dir["app/assets/javascripts/*"]
+array_to_slice.each do |x|
+  x.slice! "app/assets/javascripts/"
+end
+Rails.application.config.assets.precompile += array_to_slice
+array_to_slice = Dir["app/assets/stylesheets/*"]
+array_to_slice.each do |x|
+  x.slice! "app/assets/stylesheets/"
 end
 Rails.application.config.assets.precompile += array_to_slice
