157a9ce2691a801ec0ba4c5350e2556635f2fa17
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-12-05 18:46:41 +0100

    assets bug fixed4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,8 +3,21 @@
 Rails.application.config.assets.precompile += ['foundation-icons.css','planification.js', 'planification.css']
 
 #Foundation Icons
-Rails.application.config.assets.precompile += Dir["app/assets/stylesheets/foundation-icons/*"].map {|i| i.slice! "app/"}
+array_to_slice = Dir["app/assets/stylesheets/foundation-icons/*"]
+array_to_slice.each do |x|
+  x.slice! "app/assets/stylesheets/foundation-icons/"
+end
+
+Rails.application.config.assets.precompile += array_to_slice
 
 #Vendor
-Rails.application.config.assets.precompile += Dir["vendor/assets/javascripts/*"].map {|i| i.slice! "vendor/"}
-Rails.application.config.assets.precompile += Dir["vendor/assets/stylesheets/*"].map {|i| i.slice! "vendor/"}
+array_to_slice = Dir["vendor/assets/javascripts/*"]
+array_to_slice.each do |x|
+  x.slice! "vendor/assets/javascripts/"
+end
+Rails.application.config.assets.precompile += array_to_slice
+array_to_slice = Dir["vendor/assets/stylesheets/*"]
+array_to_slice.each do |x|
+  x.slice! "vendor/assets/stylesheets/"
+end
+Rails.application.config.assets.precompile += array_to_slice
