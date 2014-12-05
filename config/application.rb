863717733f720a8a89ab562b5162942eb837e8a8
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-12-05 11:55:20 +0100

    assets bug fixed4

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-#require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -24,6 +24,6 @@ module Amt
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w( planification.js )
+    config.assets.precompile += %w( assets/javascripts/planification.js )
   end
 end
